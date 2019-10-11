de7bfbf0820e2609e8e338e1c754460f8831d7fd
Cael Erickson <cael.e988@stu.nebo.edu>
2019-10-11 10:26:57 -0600

    animal spawn and randomization, camera settings

--- Create With Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Create With Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBound = 30f;
+    public float lowerBound = -10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,10 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
