2c8f3c500635e390cb8bbc2330a60276de8fc6d5
Cael Erickson <cael.e988@stu.nebo.edu>
2019-11-06 10:27:59 -0700

    Prototype 3, player jump, gravity manipulation, start of detect ground

---
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool canJump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb.GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (canJump == true)
+            {
+                playerRb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                canJump = false;
+            }
+        }
+    }
+}
