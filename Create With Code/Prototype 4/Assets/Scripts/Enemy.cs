a972c8d382a7662ba7c115495c1c584166d75591
Cael Erickson <cael.e988@stu.nebo.edu>
2019-12-11 10:28:54 -0700

    Prototype 4 to powerup cooldown (next is lesson 4.3 #7 (add a powerup indicator))

---
+++ Create With Code/Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody enemyRb;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = player.transform.position - transform.position.normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
